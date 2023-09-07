5dbfa2d1c4a5b0b6bff1ea6dc65dac47991db708
mrtuzl <mertuzel666@gmail.com>
2023-09-07 16:37:16 +0300

    added meta tags

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import Header from './Header.js';
 import Nav from './Nav.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import metaimg from '../src/Logo.svg';
 
 
 function App() {
   return (
       <> 
+     <meta name="description" content="Little Lemon Restaurant Webpage"/>
+     <meta name="og:title" content="Little Lemon Restaurant"/>
+     <meta name="og:description" content=""/>
+     <meta name="og:image" content={metaimg}/>
      
       <Header/>
       <Nav/>
