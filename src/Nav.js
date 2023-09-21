2b09fa6262814f58ff9dbc4e9ec44818238c8316
mrtuzl <mertuzel666@gmail.com>
2023-09-21 21:55:45 +0300

    added hamburger menu

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,25 @@
 import logo from '../src/Logo.svg';
 import { NavLink} from 'react-router-dom';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Nav() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
   
       <div className='navContainer'> 
       <div className="nav"> 
-          <img src={logo} alt="little-lemon-logo"/>
 
+          <img src={logo} alt="little-lemon-logo"/>
+         
+        <ul className={` ${isMenuOpen ? 'open' : ''}`}>
         
-          <ul className="active">
             <li> <NavLink to="/"> Home </NavLink>  </li>
             <li> <NavLink to="/"> About </NavLink> </li>
             <li> <NavLink to="/"> Menu </NavLink>  </li>
@@ -17,7 +27,11 @@ function Nav() {
             <li> <NavLink to="/"> Order Online </NavLink></li>
             <li> <NavLink to="/"> Login </NavLink></li>
           </ul>
-        
+
+         <button className="menuButton" onClick={toggleMenu}>
+      {isMenuOpen ? <FaTimes size={25} color='#495E57'/> : <FaBars size={25} color='#495E57'/>}
+        </button>
+
       </div>
       </div>
     
