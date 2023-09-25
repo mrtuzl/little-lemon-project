415ad55c58e57d37887299ba363c1744229a5283
mrtuzl <mertuzel666@gmail.com>
2023-09-25 22:16:04 +0300

    final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './Nav.js';
 import Footer from './Footer.js';
 import metaimg from '../src/Logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
@@ -12,15 +12,14 @@ function App() {
   return (
     <Router>
     <Nav/>
+    <Homepage/>
    
       <div className="container"> 
      <meta name="description" content="Little Lemon Restaurant Webpage"/>
      <meta name="og:title" content="Little Lemon Restaurant"/>
      <meta name="og:description" content=""/>
      <meta name="og:image" content={metaimg}/>
-     <Routes> 
-     <Route path="/" element={<Homepage/>}> </Route>
-     </Routes>
+    
     
     <Footer/>
     </div>
